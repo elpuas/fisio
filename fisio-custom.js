a48eac62a3af592b22bbd204cd052198cf12f15a
el Puas <elpuas@gmail.com>
2018-02-23 16:36:49 -0500

    Let's Split This into spans

--- fisio-custom.js
+++ fisio-custom.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function( $ ) {
-
+  //
   function waitForElement(elementPath, callBack){
       window.setTimeout(function(){
         if($(elementPath).length){
@@ -10,9 +10,13 @@ jQuery(document).ready(function( $ ) {
       },500)
     }
     waitForElement(".fisioEventsDate",function(){
-    console.log("Cargo Fisio");
-      var eventsDate = $(".fisioEventsDate p").text();
+      console.log("Cargo Fisio");
+      var eventsDate = $(".fisioEventsDate p").text().replace('/', '');
       console.log(eventsDate);
+      console.log(this);
+      eventsDate.html(function(i, v) {
+        return v.replace(/(\d)/g, '<span>$1</span>');
+      });
     });
 
  /**
