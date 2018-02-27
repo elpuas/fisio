7ecb64d3c79a4c2bbab178e2a58dff94c9e0212e
el Puas <elpuas@gmail.com>
2018-02-26 19:06:59 -0500

    Mobile Version

--- fisio-custom.js
+++ fisio-custom.js
@@ -18,60 +18,13 @@ jQuery(document).ready(function( $ ) {
       });
       waitForElement("#layerslider_1", function(){
         if( viewportWidth < 425 ) {
-          console.log('is small than 425px');
           $("#layerslider_1").css("height", viewportHeight);
         }
-        console.log("width: " + viewportWidth + "\n" + "Height: " + viewportHeight );
         $(window).resize(function() {
           if( viewportWidth < 425 ) {
             $("#layerslider_1").css("height", viewportHeight);
           }
         });
       });
-
-
-
-    /*
-    waitForElement(".fisioEventsDate",function(){
-
-      console.log("Cargo Fisio");
-      var eventsDate = $(".fisioEventsDate").text().slice();
-      console.log("Sin Separator: " + eventsDate + "\n");
-
-
-    var whiteBackground = $('.fisio-prof__grid--container > .vc_column-inner');
-
-    if( whiteBackground.length > 0 ) {
-            // console.log('Si Existe');
-            // Select and loop the container element of the elements you want to equalise
-            whiteBackground.each(function(){
-
-              // Cache the highest
-              var highestBox = 0;
-
-              // Select and loop the elements you want to equalise
-              $('.wpb_wrapper:first-child', this).each(function(){
-                console.log(this);
-
-                // If this box is higher than the cached highest then store it
-                if($(this).height() > highestBox) {
-                  highestBox = $(this).height();
-                  console.log('esta es la mas larga:' + highestBox);
-                }
-
-              });
-
-              // Set the height of all those children to whichever was highest
-              $('.fisio-prof__grid--container > .vc_column-inner  > .wpb_wrapper').css({"height" : (highestBox + "px")});
-
-            });
-        }
-      });
-
- /**
-   function( eventsDateMod ) {
-   eventsDate = $('.fisioEventsDate p').text();
-   console.log(eventsDate);
-  });
-  */
+ console.log("%c Made with  💖 and a lot of  ☕ by el.puas | https://elpuas.com ", "color:#fff;background:#000;");
 });
